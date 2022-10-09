aae923b6f411450aa228557d53d581abddc01e45
emlahieu98 <hieucr98tb@gmail.com>
2022-10-09 17:51:10 +0700

    fix: delete

--- controllers/post.js
+++ controllers/post.js
@@ -1,168 +1,146 @@
-const mongoose = require("mongoose");
-const Joi = require("joi");
-const { transValidation } = require("../langs/errors/vn");
-const postModel = require("../models/post");
-const { getItems } = require("../utils/paginate")
-const _ = require("lodash")
+const mongoose = require('mongoose')
+const Joi = require('joi')
+const { transValidation } = require('../langs/errors/vn')
+const postModel = require('../models/post')
+const { getItems } = require('../utils/paginate')
+const _ = require('lodash')
 
 exports.getPosts = async (req, res) => {
-  try {
-    const { page, page_size, type, category_name, cateChild_name } = await Joi.object()
-      .keys({
-        page: Joi.string().default(1),
-        page_size: Joi.string().default(5),
-        type: Joi.string(),
-      })
-      .validateAsync(req.query, { stripUnknown: true })
+    try {
+        const { page, page_size } = await Joi.object()
+            .keys({
+                page: Joi.string().default(1),
+                page_size: Joi.string().default(5),
+                type: Joi.string(),
+            })
+            .validateAsync(req.query, { stripUnknown: true })
 
-    const result = await getItems(postModel, page, page_size)
+        const result = await getItems(postModel, page, page_size)
 
-    return res.status(200).json({
-      status: "success",
-      data: result.docs,
-      totalDocs: result.totalDocs,
-      currentPage: result.page,
-      totalPages: result.totalPages,
-    })
-  } catch (error) {
-    return res.status(400).json({
-      status: "fail",
-      message: error.message,
-    });
-  }
-
-};
+        return res.status(200).json({
+            status: 'success',
+            data: result.docs,
+            totalDocs: result.totalDocs,
+            currentPage: result.page,
+            totalPages: result.totalPages,
+        })
+    } catch (error) {
+        return res.status(400).json({
+            status: 'fail',
+            message: error.message,
+        })
+    }
+}
 
 exports.createPost = async (req, res) => {
-  try {
-    const value = await Joi.object()
-      .keys({
-        title: Joi.string().required(),
-        image: Joi.string(),
-        content: Joi.string().required(),
-        slug: Joi.string().min(6).required(),
-      })
-      .validateAsync(req.body, { stripUnknown: true })
+    try {
+        const value = await Joi.object()
+            .keys({
+                title: Joi.string().required(),
+                image: Joi.string(),
+                content: Joi.string().required(),
+                slug: Joi.string().required(),
+            })
+            .validateAsync(req.body, { stripUnknown: true })
 
-    const slug = await postModel.findOne({ slug: value.slug })
-    if (slug) {
-      return res.status(400).json({
-        status: "fail",
-        message: transValidation.slug_exist,
-      });
+        const slug = await postModel.findOne({ slug: value.slug })
+        if (slug) {
+            return res.status(400).json({
+                status: 'fail',
+                message: transValidation.slug_exist,
+            })
+        }
+
+        await postModel.create(value)
+
+        return res.status(200).json({
+            status: 'success',
+            message: transValidation.input_correct,
+            data: value,
+        })
+    } catch (error) {
+        return res.status(400).json({
+            status: 'fail',
+            message: error.message,
+        })
     }
-
-    await postModel.create(value)
-
-    return res.status(200).json({
-      status: "success",
-      message: transValidation.input_correct,
-      data: value,
-    });
-  } catch (error) {
-    return res.status(400).json({
-      status: "fail",
-      message: error.message,
-    });
-  }
-};
+}
 
 exports.getPostDetail = async (req, res) => {
-  try {
-    const { id } = req.params
-    const post = await postModel.findOne({ slug: id })
-    if (_.isEmpty(post)) {
-      return res.status(401).json({
-        status: "fail",
-        message: transValidation.not_found,
-      });
+    try {
+        const { id } = req.params
+        const post = await postModel.findOne({ slug: id })
+        if (_.isEmpty(post)) {
+            return res.status(401).json({
+                status: 'fail',
+                message: transValidation.not_found,
+            })
+        }
+
+        return res.status(200).json({
+            status: 'success',
+            data: post,
+        })
+    } catch (error) {
+        return res.status(400).json({
+            status: 'fail',
+            message: error.message,
+        })
     }
-
-    return res.status(200).json({
-      status: "success",
-      data: post
-    });
-
-  } catch (error) {
-    return res.status(400).json({
-      status: "fail",
-      message: error.message,
-    });
-  }
-};
+}
 
 exports.deletePost = async (req, res) => {
-  try {
-    const { id } = req.params
-    const post = await postModel.findById(id)
-    if (!post) {
-      return res.status(401).json({
-        status: "fail",
-        message: transValidation.not_found,
-      });
+    try {
+        const { id } = req.params
+        const post = await postModel.findById(id)
+        if (!post) {
+            return res.status(401).json({
+                status: 'fail',
+                message: transValidation.not_found,
+            })
+        }
+
+        await postModel.findByIdAndDelete(id)
+
+        return res.status(200).json({
+            status: 'success',
+            message: transValidation.input_correct,
+        })
+    } catch (error) {
+        return res.status(400).json({
+            status: 'fail',
+            message: error.message,
+        })
     }
-
-    await postModel.findByIdAndDelete(id)
-
-    return res.status(200).json({
-      status: "success",
-      message: transValidation.input_correct,
-    });
-
-  } catch (error) {
-    return res.status(400).json({
-      status: "fail",
-      message: error.message,
-    });
-  }
-};
+}
 exports.editPost = async (req, res) => {
-  const { id } = req.params
-  try {
+    const { id } = req.params
+    try {
+        const value = await Joi.object()
+            .keys({
+                title: Joi.string(),
+                image: Joi.string(),
+                content: Joi.string(),
+                slug: Joi.string(),
+            })
+            .validateAsync(req.body, { stripUnknown: true })
+        const post = await postModel.findOne({ slug: id })
+        if (!post) {
+            return res.status(401).json({
+                status: 'fail',
+                message: transValidation.not_found,
+            })
+        }
+        await postModel.findByIdAndUpdate(post._id, value)
 
-    const value = await Joi.object()
-      .keys({
-        title: Joi.string(),
-        image: Joi.string(),
-        content: Joi.string(),
-        slug: Joi.string().min(6),
-        description: Joi.string(),
-        categoryId: Joi.string(),
-        cateChildId: Joi.string(),
-        tags: Joi.array().items(Joi.string()),
-      })
-      .validateAsync(req.body, { stripUnknown: true })
-    const post = await postModel.findOne({ slug: id })
-    if (!post) {
-      return res.status(401).json({
-        status: "fail",
-        message: transValidation.not_found,
-      });
+        return res.status(200).json({
+            status: 'success',
+            message: transValidation.input_correct,
+        })
+    } catch (error) {
+        return res.status(400).json({
+            status: 'fail',
+            message: error.message,
+        })
     }
-    const isSlugExist = await postModel.findOne({
-      slug: value.slug, _id: {
-        $ne: post._id
-      }
-    });
-    if (isSlugExist) {
-      return res.status(400).json({
-        status: "fail",
-        message: transValidation.slug_exist,
-      });
-    }
-
-    value.tags = await checkTag(value.tags)
-
-    await postModel.findByIdAndUpdate(post._id, value)
-
-    return res.status(200).json({
-      status: "success",
-      message: transValidation.input_correct,
-    });
-  } catch (error) {
-    return res.status(400).json({
-      status: "fail",
-      message: error.message,
-    });
-  }
-};
+}
