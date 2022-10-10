abd5d7d2e167793649646547a9f035f37bce0d2f
emlahieu98 <hieucr98tb@gmail.com>
2022-10-10 13:57:31 +0700

    fix:db

--- config/db.js
+++ config/db.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose')
 module.exports = connect = async () => {
-
     try {
         const response = await mongoose.connect(process.env.MONGO_URI)
-        if (response) console.log('Connect mongo db success');
-
+        if (response) console.log('Connect mongo database success')
     } catch (error) {
-        console.log(error.message);
+        console.log(error.message)
     }
-
 }
